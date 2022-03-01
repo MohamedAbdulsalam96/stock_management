80ba1da5e5903e879563aab7595852f5d85a37ad
rahib-hassan <rahib.hassan10@gmail.com>
2022-03-01 12:03:50 +0530

    feat: create stock entry on item creation

--- stock_management/stock_management/doctype/item/item.py
+++ stock_management/stock_management/doctype/item/item.py
@@ -1,8 +1,33 @@
 # Copyright (c) 2022, Rahib Hassan and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.website.website_generator import WebsiteGenerator
+from frappe.utils import today, now
+from frappe import _
 
 class Item(WebsiteGenerator):
-	pass
+
+	def validate(self):
+		if self.maintain_stock == 1 and self.opening_stock == 0:
+			frappe.throw(_("Opening Stock is required for stock item."))
+
+	def create_stock_entry(self):
+		stock = frappe.new_doc('Stock Entry')
+		stock.title = "Material Receipt"
+		stock.posting_date = today()
+		stock.posting_time = now()
+		stock.stock_entry_type = "Material Receipt"
+		stock.append('items', {
+			'item_code': self.item_code,
+			'target_warehouse': self.default_warehouse,
+			'quantity': self.opening_stocks,
+			'rate': self.valuation_rate,
+			'amount': quantity * rate
+			})
+		stock.insert()
+		stock.submit()
+
+	def after_insert(self):
+		if self.maintain_stock == 1:
+			self.create_stock_entry()
