0149e10044ae19aede162f607c78c007f223e09f
rahib-hassan <rahib.hassan10@gmail.com>
2022-03-08 10:22:23 +0530

    feat: unittest to create ledger entry

--- stock_management/stock_management/doctype/stock_entry/test_stock_entry.py
+++ stock_management/stock_management/doctype/stock_entry/test_stock_entry.py
@@ -1,8 +1,29 @@
 # Copyright (c) 2022, Rahib Hassan and Contributors
 # See license.txt
 
-# import frappe
+import frappe
 import unittest
 
+def create_stock_entry():
+	se = frappe.get_doc({
+		'doctype': 'Stock Entry',
+		'stock_entry_type': 'Material Receipt',
+		'items': [{
+			'target_warehouse': 'Stores',
+			'item_code': 'PROD - 001',
+			'quantity': 100
+		}]
+	}).submit()
+	return se
+
 class TestStockEntry(unittest.TestCase):
-	pass
+	def setUp(self):
+		print("Setting up stocks entry")
+
+	def tearDown(self):
+		frappe.db.rollback()
+		frappe.set_user("Administrator")
+	
+	def test_ledger_posting(self):
+		doc = create_stock_entry()
+		sle = frappe.db.get_value('Stock Ledger Entry', {'voucher_no': doc.name}, 'name')
