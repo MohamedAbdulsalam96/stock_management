e33287b1e9b1a71ccb5e0882d7cbe92bfe1a57a7
rahib-hassan <rahib.hassan10@gmail.com>
2022-03-11 15:50:01 +0530

    fix: removed maintatin_stock and valuation based on it

--- stock_management/stock_management/doctype/item/item.py
+++ stock_management/stock_management/doctype/item/item.py
@@ -9,20 +9,15 @@ from frappe import _
 class Item(WebsiteGenerator):
 
 	def validate(self):
-		if self.maintain_stock == 1 and self.opening_stock == 0:
-			frappe.throw(_("Opening Stock is required for stock item."))
-		if self.maintain_stock == 1 and self.valuation_rate == 0:
-			frappe.throw(_("Valuation Rate is required for stock item."))
-
 		if not self.item_name:
 			self.item_name = self.item_code
 
 	def create_stock_entry(self):
 		stock = frappe.new_doc('Stock Entry')
-		stock.title = "Material Receipt"
+		stock.title = 'Material Receipt'
 		stock.posting_date = today()
 		stock.posting_time = now()
-		stock.stock_entry_type = "Material Receipt"
+		stock.stock_entry_type = 'Material Receipt'
 		stock.append('items', {
 			'item_code': self.item_code,
 			'target_warehouse': self.default_warehouse,
@@ -30,9 +25,9 @@ class Item(WebsiteGenerator):
 			'rate': self.valuation_rate,
 			'amount': self.opening_stock * self.valuation_rate
 			})
-		stock.docstatus = 1
 		stock.insert()
+		stock.submit()
 
 	def after_insert(self):
-		if self.maintain_stock == 1:
+		if self.opening_stock:
 			self.create_stock_entry()
