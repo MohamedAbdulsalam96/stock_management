dc1a587f65f95c08c52ae482a122c370641d99c0
rahib-hassan <rahib.hassan10@gmail.com>
2022-03-11 14:53:09 +0530

    fix: removed hardcoded values

--- stock_management/add_warehouse.py
+++ stock_management/add_warehouse.py
@@ -1,18 +1,16 @@
 import frappe
 
 def create_default_warehouse():
-    create_warehouse('All Warehouse')
-    create_warehouse('Stores')
-    create_warehouse('Finished Goods')
-    create_warehouse('Work In Progress')
+    create_warehouse('All Warehouse', is_group=1)
+    create_warehouse('Stores', parent_warehouse='All Warehouse')
+    create_warehouse('Finished Goods', parent_warehouse='All Warehouse')
+    create_warehouse('Work In Progress', parent_warehouse='All Warehouse')
 
-def create_warehouse(warehouse_name):
+def create_warehouse(warehouse_name, is_group=0, parent_warehouse=None):
     warehouse = frappe.get_doc({
         'doctype': 'Warehouse',
         'warehouse_name': warehouse_name,
-        'is_group': 1,
+        'is_group': is_group,
+        'parent_warehouse': parent_warehouse
     })
-
-    if warehouse_name != 'All Warehouse':
-            warehouse.parent_warehouse = 'All Warehouse'
     warehouse.insert()
