0080ae7b7849649f1c778e4dce7e35474e96d02a
rahib-hassan <rahib.hassan10@gmail.com>
2022-03-18 17:45:11 +0530

    feat: get records and calculate valuation rate

--- stock_management/stock_management/doctype/stock_entry/calculate_valuation_rate.py
+++ stock_management/stock_management/doctype/stock_entry/calculate_valuation_rate.py
@@ -1,21 +1,23 @@
 import frappe
+import json
 
 @frappe.whitelist()
-def get_valuation_rate(item_code, source_warehouse):
-    records = frappe.db.get_list('Stock Ledger Entry', 
-        filters={'item_code': item_code, 'warehouse': source_warehouse},
-        fields=['incoming_rate', 'actual_qty', 'warehouse', 'item_code', 'amount'])
+def get_valuation_rate(records):
+    if isinstance(records, str):
+        records = json.loads(records)
     if records:
-        return calculate_valuation_rate(records)
+        total_qty = 0
+        total_amount = 0
 
-def calculate_valuation_rate(records):
-    
-    total_qty = 0
-    total_amount = 0
-    for record in records:
-        total_qty += record.actual_qty
-        total_amount += record.amount
+        print(records[0])
 
-    valuation_rate = total_amount / total_qty
-    print(valuation_rate)
-    return valuation_rate
+        for record in records:
+            total_qty += record['actual_qty']
+            total_amount += record['amount']
+        
+        valuation_rate = total_amount / total_qty
+        return valuation_rate
+
+def get_records(item, warehouse):
+    records = frappe.db.get_list('Stock Ledger Entry', filters={'item_code': item, 'warehouse': warehouse}, fields=['*'])
+    return records
