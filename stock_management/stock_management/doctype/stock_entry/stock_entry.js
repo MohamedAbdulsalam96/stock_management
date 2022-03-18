eab780577904458bc86ef46ce751e4a9ef3f7476
rahib-hassan <rahib.hassan10@gmail.com>
2022-03-18 17:47:32 +0530

    fix: caluation rate calculation only for if source warehouse

--- stock_management/stock_management/doctype/stock_entry/stock_entry.js
+++ stock_management/stock_management/doctype/stock_entry/stock_entry.js
@@ -3,19 +3,27 @@
 
 frappe.ui.form.on("Stock Entry Item", "item_code", function(frm, cdt, cdn)
 {
-    console.log("Hello")
+    // console.log("Hello eMan")
     var row = locals[cdt][cdn]
-    frappe.call
-    ({
-        "method": "stock_management.stock_management.doctype.stock_entry.calculate_valuation_rate.get_valuation_rate",
-        "args": 
-        {
-            "item_code": row.item_code, 
-            "source_warehouse": row.source_warehouse
-        },
-        callback: function(r){
-            console.log(r.message)
-            frappe.model.set_value(cdt, cdn, "rate", r.message);
-        }
-    });
+    if (row.source_warehouse)
+    {
+        var sle_entries = frappe.db.get_list('Stock Ledger Entry', {
+            fields: ['actual_qty', 'amount'],
+            filters: {
+                item_code: row.item_code,
+                warehouse: row.source_warehouse
+            }
+        }).then(e => {console.log(e)
+            frappe.call({
+                method: "stock_management.stock_management.doctype.stock_entry.calculate_valuation_rate.get_valuation_rate",
+                args: {
+                    records: e
+                },
+                callback: function(r){
+                    // console.log(r.message)
+                    frappe.model.set_value(cdt, cdn, "rate", r.message);
+                }
+            })
+        });
+    }
 });
