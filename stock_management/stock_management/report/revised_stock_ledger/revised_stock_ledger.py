6e589363378d777dbe07886dbe9f366559d169d7
rahib-hassan <rahib.hassan10@gmail.com>
2022-03-18 17:43:51 +0530

    feat: revised data structure for stock ledger report

---
+++ stock_management/stock_management/report/revised_stock_ledger/revised_stock_ledger.py
@@ -0,0 +1,134 @@
+# Copyright (c) 2022, Rahib Hassan and contributors
+# For license information, please see license.txt
+
+import frappe
+from frappe import _
+from stock_management.stock_management.doctype.stock_entry.calculate_valuation_rate import get_records, get_valuation_rate
+
+def execute(filters=None):
+	
+	rows = get_data(filters)
+	columns = get_columns()
+
+	return columns, rows
+
+def get_columns():
+	return [
+		{
+			'fieldname': 'posting_date',
+			'label': _('Posting Date')
+		},
+		{
+			'fieldname': 'posting_time',
+			'label': _('Posting Time')
+		},
+		{
+			'fieldname': 'item_code',
+			'label': _('Item'),
+			'fieldtype': 'Link',
+			'options': 'Item',
+			'width': 110
+		},
+		{
+			'fieldname': 'warehouse',
+			'label': _('Warehouse'),
+			'fieldtype': 'Link',
+			'options': 'Warehouse',
+		},
+		{
+			'fieldname': 'actual_qty',
+			'label': _('Quantity'),
+			'width': 110
+		},
+		{
+			'fieldname': 'updated_qty',
+			'label': _('Quantity after transaction'),
+			'fieldtype': 'Float',
+			'width': 110
+		},
+		{
+			'fieldname': 'incoming_rate',
+			'label': _('Rate'),
+			'width': 110
+		},
+		{
+			'fieldname': 'valuation_rate',
+			'label': _('Valuation Rate'),
+			'width': 120
+		},
+		{
+			'fieldname': 'amount',
+			'label': _('Stock Value'),
+			'fieldtype': 'Currency',
+			'width': 110
+		},
+		{
+			'fieldname': 'stock_value_difference',
+			'label': _('Stock Value Difference'),
+			'fieldtype': 'Currency',
+			'width': 110
+		},
+		{
+			'fieldname': 'voucher_type',
+			'label': _('Voucher Type'),
+			'fieldtype': 'Link',
+			'options': 'Doctype'
+		},
+		{
+			'fieldname': 'voucher_no',
+			'label': _('Voucher No.'),
+			'fieldtype': 'Link',
+			'options': 'Stock Ledger Entry',
+			'width': 160
+		},
+	]
+	
+def get_data(filters):
+	all_filters = [['docstatus', '=', 1]]
+	orm_filters = get_conditions(filters, all_filters)
+
+	records = frappe.db.get_list('Stock Ledger Entry', filters=orm_filters, order_by='posting_time desc',
+		fields=['item_code', 'warehouse', 'posting_date', 'posting_time', 'actual_qty', 'incoming_rate', 'amount', 'voucher_type', 'voucher_no'])
+	
+	data = process_data(records)
+
+	return data
+
+def get_conditions(filters, all_filters):
+	conditions = ""
+	if filters.get("item_code"):
+		all_filters.append(['item_code', '=', filters.get('item_code')])
+		
+	if filters.get("warehouse"):
+		all_filters.append(['item_code', '=', filters.get('warehouse')])
+	
+	return all_filters
+
+def process_data(records):
+	rows = []
+	processed_data = {}
+	for record in records:
+		combination = (record['item_code'], record['warehouse'])
+		
+		if combination in processed_data:
+			comb_values = processed_data[combination]
+			# print(processed_data)
+			sle_entries = get_records(combination[0], combination[1])
+			valuation_rate = get_valuation_rate(sle_entries)
+
+			record['valuation_rate'] = round(valuation_rate, 2)
+			record['stock_value'] = comb_values[-1]['amount']
+			record['stock_value_difference'] = comb_values[-1]['amount'] - record['amount']
+			record['updated_qty'] = comb_values[-1]['actual_qty'] + record['actual_qty']
+				
+			processed_data[combination].append(record)
+			rows.append(record)
+		else:
+			processed_data[combination] = [record]
+			processed_data[combination][-1].update({'stock_value_difference': record['amount'], 'updated_qty': record['actual_qty'], 'valuation_rate': record['incoming_rate']})
+			rows.append(record)
+		
+	print(processed_data)
+
+	return rows
+
